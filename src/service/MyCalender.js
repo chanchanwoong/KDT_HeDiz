7868678f7260c55e82aa292258ac95674210026c
MyoungJin Yang <audwls6191@naver.com>
2024-01-10 21:03:49 +0900

    Feat: 휴무일페이지 기능 추가

--- src/service/MyCalender.js
+++ src/service/MyCalender.js
@@ -4,15 +4,34 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 class MyCalendar extends Component {
   render() {
+    const { parsedDate } = this.props;
+    console.log({ parsedDate });
+
+    const events = [
+      { title: '이주 휴무일', date: '2024-01-25' },
+      { title: '루하 휴무일', start: '2024-01-22', end: '2024-01-23' },
+    ];
+
+    if (parsedDate) {
+      let start = parsedDate.parsedStartDate;
+      let end = parsedDate.parsedEndDate;
+      console.log(start, end);
+      console.log('실행');
+      events.push({
+        title: '미용실 휴무',
+        start: start,
+        end: end,
+      });
+      console.log(start, end);
+    }
+    console.log(events);
     return (
       <div className='App'>
         <FullCalendar
           defaultView='dayGridMonth'
           plugins={[dayGridPlugin]}
-          events={[
-            { title: '이주 휴무일', date: '2024-01-25' },
-            { title: '루하 휴무일', date: '2024-01-22' },
-          ]}
+          events={events}
+          displayEventTime={false}
         />
       </div>
     );
