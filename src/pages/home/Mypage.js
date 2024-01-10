38e32c584a56fb92d9c535c9fef246393bbea7d2
MyoungJin Yang <audwls6191@naver.com>
2024-01-10 13:05:38 +0900

    Feat: 마이페이지 추가

--- src/pages/home/Mypage.js
+++ src/pages/home/Mypage.js
@@ -1,7 +1,164 @@
+import { useEffect, useState, useRef } from 'react';
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { MultiSelect } from 'primereact/multiselect';
+import { Calendar } from 'primereact/calendar';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
+import { Panel } from 'primereact/panel';
+import { Avatar } from 'primereact/avatar';
+import { InputText } from 'primereact/inputtext';
+import { Image } from 'primereact/image';
+import img from '../../assets/img.jpg';
+
 function Mypage() {
   return (
     <>
-      <h1>마이페이지</h1>
+      <Panel
+        toggleable
+        header='프로필'
+      >
+        <div className='flex'>
+          <div className='flex flex-column flex-wrap gap-4 w-4'>
+            <div>
+              <Image
+                className='mr-5'
+                src={img}
+                alt='Image'
+                width='70%'
+                height='100%'
+              />
+              <Button label='프로필 사진 수정' />
+            </div>
+          </div>
+          <div className='flex flex-column flex-wrap gap-4 w-8'>
+            <div className='p-inputgroup flex-1'>
+              <span className='p-inputgroup-addon'>
+                <i className='pi pi-user mr-2'></i>
+              </span>
+              <InputText placeholder='소속' />
+            </div>
+            <div className='p-inputgroup flex-1'>
+              <span className='p-inputgroup-addon'>
+                <i className='pi pi-user mr-2'></i>
+              </span>
+              <InputText placeholder='닉네임' />
+            </div>
+            <div className='p-inputgroup flex-1'>
+              <span className='p-inputgroup-addon'>
+                <i className='pi pi-comment mr-2'></i>
+              </span>
+              <InputText
+                className='min-w-min md:min-w-max'
+                placeholder='소개'
+              />
+            </div>
+            <div className='p-inputgroup flex-1'>
+              <span className='p-inputgroup-addon'>
+                <i className='pi pi-clock'></i>
+              </span>
+              <InputText
+                name='shop_start'
+                placeholder='근무 시작시간'
+              />
+            </div>
+
+            <div className='p-inputgroup flex-1 mb-5'>
+              <span className='p-inputgroup-addon'>
+                <i className='pi pi-clock'></i>
+              </span>
+              <InputText
+                name='shop_end'
+                placeholder='근무 종료시간'
+              />
+            </div>
+            <Button label='수정하기' />
+          </div>
+        </div>
+      </Panel>
+
+      <div className='flex col-12'>
+        <div className='col-6'>
+          <Panel
+            toggleable
+            header='계정'
+          >
+            <div className='flex flex-column flex-wrap gap-4'>
+              <div className='p-inputgroup flex-1'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-user mr-2'></i>
+                </span>
+                <InputText placeholder='이름' />
+              </div>
+              <div className='p-inputgroup flex-1'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-user mr-2'></i>
+                </span>
+                <InputText placeholder='아이디' />
+              </div>
+              <div className='p-inputgroup flex-1'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-comment mr-2'></i>
+                </span>
+                <InputText
+                  className='min-w-min md:min-w-max'
+                  placeholder='전화번호'
+                />
+              </div>
+              <div className='p-inputgroup'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-clock'></i>
+                </span>
+                <InputText
+                  name='shop_start'
+                  placeholder='권한'
+                />
+              </div>
+              <Button label='수정하기' />
+            </div>
+          </Panel>
+        </div>
+        <div className='col-6'>
+          <Panel
+            toggleable
+            header='비밀번호'
+          >
+            <span className='ml-5'>비밀번호 변경</span>
+            <div className='flex flex-column gap-4 ml-5'>
+              <div className='p-inputgroup'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-clock'></i>
+                </span>
+                <InputText
+                  name='shop_start'
+                  placeholder='현재 비밀번호'
+                />
+              </div>
+              <div className='p-inputgroup'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-clock'></i>
+                </span>
+                <InputText
+                  name='shop_start'
+                  placeholder='새 비밀번호'
+                />
+              </div>
+              <div className='p-inputgroup'>
+                <span className='p-inputgroup-addon'>
+                  <i className='pi pi-clock'></i>
+                </span>
+                <InputText
+                  name='shop_start'
+                  placeholder='새 비밀번호 확인'
+                />
+              </div>
+              <Button label='수정하기' />
+            </div>
+          </Panel>
+        </div>
+      </div>
     </>
   );
 }
