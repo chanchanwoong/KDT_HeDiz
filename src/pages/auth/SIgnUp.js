e876b1b6b2e7d4582bf21cc0a917002857db5606
dev2gahui <dev2gahui@gmail.com>
2024-01-11 20:55:42 +0900

    Feat: 일반 회원가입 컴포넌트 수정

--- src/pages/auth/SIgnUp.js
+++ src/pages/auth/SIgnUp.js
@@ -1,87 +1,93 @@
 import { useState } from 'react';
-// import { Form, useNavigate, json, redirect, Link } from 'react-router-dom';
-// import Logo from 'components/common/Logo';
 
-import { Form, Link } from 'react-router-dom';
+import Logo from 'components/common/Logo';
+import { Form } from 'react-router-dom';
 import { InputText } from 'primereact/inputtext';
 import { InputNumber } from 'primereact/inputnumber';
 import { Password } from 'primereact/password';
-import { Checkbox } from 'primereact/checkbox';
+import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
 
 function SignUp() {
+  const [value, setValue] = useState('');
+
   return (
     <main className='flex flex-column bg-white p-6 w-auto border-round-lg'>
-      {/* 로고 컴포넌트 분리 필요 */}
-      <span className='flex align-items-center justify-content-center gap-1 mb-4'>
-        <span className='font-medium text-4xl font-bold'>
-          He<span className='text-primary'>Diz</span> - 회원가입
-        </span>
-      </span>
+      <Logo />
 
       <Form
         // method='post'
         action='/'
-        className='flex flex-column flex-wrap gap-4'
+        className='flex flex-column flex-wrap gap-3'
       >
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>미용실 고유코드</label>
-          <div className='p-inputgroup flex-1'>
-            <InputNumber placeholder='Price' />
-            <span className='p-inputgroup-addon'>인증</span>
-          </div>
-        </div>
+        <h1>일반 회원가입</h1>
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>상호명</label>
+        <div className='p-inputgroup flex-1'>
           <InputText
-            id='userid'
-            disabled
+            placeholder='미용실 고유코드'
+            name='shop_code'
+          />
+          <Button
+            label='인증하기'
+            type='button'
           />
         </div>
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>아이디</label>
-          <InputText id='userid' />
-        </div>
+        <InputText
+          name='shop_name'
+          placeholder='상호명'
+          disabled
+        />
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='passwd'>비밀번호</label>
-          <Password
-            id='passwd'
-            feedback={false}
-            toggleMask
+        <div className='p-inputgroup flex-1'>
+          <InputText
+            name='staff_id'
+            placeholder='아이디'
+          />
+          <Button
+            label='중복확인'
+            type='button'
           />
         </div>
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='passwd'>비밀번호 확인</label>
-          <Password
-            id='passwd'
-            feedback={false}
-            toggleMask
-          />
-        </div>
+        <Password
+          name='staff_pw'
+          placeholder='비밀번호'
+          feedback={false}
+          toggleMask
+        />
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>성함</label>
-          <InputText id='userid' />
-        </div>
+        <Password
+          name='staff_pw_check'
+          placeholder='비밀번호 확인'
+          feedback={false}
+          toggleMask
+        />
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>닉네임</label>
-          <InputText id='userid' />
-        </div>
+        <InputText
+          name='staff_name'
+          placeholder='이름'
+        />
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>전화번호</label>
-          <InputText id='userid' />
-        </div>
+        <InputText
+          name='staff_nickname'
+          placeholder='닉네임'
+        />
 
-        <div className='flex flex-column gap-2'>
-          <label htmlFor='userid'>소개</label>
-          <InputText id='userid' />
-        </div>
+        <InputNumber
+          name='staff_phone'
+          placeholder='전화번호'
+        />
+
+        <InputTextarea
+          name='staff_intro'
+          placeholder='소개글'
+          autoResize
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          rows={3}
+          cols={30}
+        />
 
         <Button
           label='회원가입'
