2a5174e0be175bea837fe9fb6131f9772c3b9b68
dev2gahui <dev2gahui@gmail.com>
2024-01-12 11:58:49 +0900

    Feat: 로그인 아이디 저장 기능 구현

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -2,11 +2,11 @@
 function Logo() {
   return (
     <>
-      <span className='md:w-15rem inline-flex align-items-center gap-1 px-3'>
+      <h1 className='flex justify-content-center align-items-center gap-1 m-0'>
         <span className='font-medium text-xl font-semibold'>
           He<span className='text-primary'>Diz</span>
         </span>
-      </span>
+      </h1>
     </>
   );
 }
--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,18 +1,36 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
-
-// import Logo from 'components/common/Logo';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import Logo from 'components/common/Logo';
 
 import { Form, Link, redirect } from 'react-router-dom';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
-import KakaoLogin from '../../components/common/KakaoLogin';
-import Logo from 'components/common/Logo';
-import axios from 'axios';
 
 function SignIn() {
-  const [checked, setChecked] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(['rememberUserId']);
+  const [userid, setUserid] = useState('');
+  const [isRemember, setIsRemember] = useState(false);
+
+  // Cookie 아이디 저장
+  useEffect(() => {
+    if (cookies.rememberUserId !== undefined) {
+      setUserid(cookies.rememberUserId);
+      setIsRemember(true);
+    }
+  }, []);
+
+  const handleOnChange = (e) => {
+    setIsRemember(e.target.checked);
+    if (!e.target.checked) {
+      removeCookie('rememberUserId');
+    } else {
+      setCookie('rememberUserId', userid, { maxAge: 5 * (60 * 60 * 24) });
+    }
+  };
 
   return (
     <main className='flex flex-column bg-white p-6 w-auto border-round-lg'>
@@ -20,20 +38,22 @@ function SignIn() {
 
       <Form
         method='post'
-        className='flex flex-column flex-wrap gap-4'
+        className='flex flex-column flex-wrap gap-4 mt-6'
       >
         <div className='flex flex-column gap-2'>
-          <label htmlFor='staff_id'>아이디</label>
           <InputText
             id='staff_id'
             name='staff_id'
+            defaultValue={userid}
+            placeholder='아이디'
+            onChange={(e) => setUserid(e.target.value)}
           />
         </div>
         <div className='flex flex-column gap-2'>
-          <label htmlFor='staff_pw'>비밀번호</label>
           <Password
             id='staff_pw'
             name='staff_pw'
+            placeholder='비밀번호'
             feedback={false}
             toggleMask
           />
@@ -41,30 +61,36 @@ function SignIn() {
         <div className='flex align-items-center justify-content-between mb-4'>
           <div className='flex align-items-center mr-8'>
             <Checkbox
-              id='rememberme'
-              onChange={(e) => setChecked(e.checked)}
-              checked={checked}
+              id='saveId'
+              onChange={(e) => handleOnChange(e)}
+              checked={isRemember}
               className='mr-2'
             />
-            <label htmlFor='rememberme'>아이디 저장</label>
+            <label htmlFor='saveId'>아이디 저장</label>
           </div>
-          <Link
-            to='/auth/find'
-            className='font-medium no-underline text-blue-500 text-right cursor-pointer'
-          >
-            아이디/비밀번호 찾기
-          </Link>
         </div>
         <Button
           label='로그인'
           type='submit'
         />
-        <KakaoLogin />
+
+        <Link
+          to='/auth/sign-up'
+          className='font-medium no-underline text-blue-500 text-left cursor-pointer'
+        >
+          일반 회원가입
+        </Link>
         <Link
           to='/auth/register'
-          className='font-medium no-underline text-black-500 text-left cursor-pointer'
+          className='font-medium no-underline text-blue-500 text-left cursor-pointer'
         >
-          미용실 등록(점주 회원가입)
+          점주 회원가입(미용실 등록)
+        </Link>
+        <Link
+          to='/auth/find'
+          className='font-medium no-underline text-blue-500 text-left cursor-pointer'
+        >
+          아이디/비밀번호 찾기
         </Link>
       </Form>
     </main>
