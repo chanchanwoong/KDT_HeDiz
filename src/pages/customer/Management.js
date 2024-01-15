f1d864fb9aa0ced7bbcdef118cd5fddc5d54fc62
MyoungJin Yang <audwls6191@naver.com>
2024-01-15 20:09:54 +0900

    Feat: 고객관리 페이지, 전체 예약 페이지 개발

--- src/pages/customer/Management.js
+++ src/pages/customer/Management.js
@@ -1,8 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import axios from 'axios';
+import { Tag } from 'primereact/tag';
+
 function Management() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/customer/1')
+      .then((response) => {
+        console.log(response.data);
+        setCustomers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const renderHeader = () => {
+    return (
+      <div className='flex flex-wrap gap-2 justify-content-between align-items-center'>
+        <h4 className='m-0'>전체 고객 목록</h4>
+        <span className='p-input-icon-left'>
+          <i className='pi pi-search' />
+        </span>
+      </div>
+    );
+  };
+
+  const header = renderHeader();
+
   return (
-    <>
-      <h1>고객 관리</h1>
-    </>
+    <div className='card'>
+      <DataTable
+        value={customers}
+        paginator
+        header={header}
+        rows={10}
+        paginatorTemplate='FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown'
+        rowsPerPageOptions={[10, 25, 50]}
+      >
+        <Column
+          field='cust_name'
+          header='고객 이름'
+          style={{ minWidth: '14rem' }}
+        />
+        <Column
+          field='cust_id'
+          header='고객 id'
+          sortable
+          style={{ minWidth: '14rem' }}
+        />
+        <Column
+          field='cust_phone'
+          header='전화번호'
+          sortable
+          style={{ minWidth: '12rem' }}
+        />
+
+        <Column
+          field='cust_gender'
+          header='성별'
+          sortable
+          dataType='date'
+          style={{ minWidth: '12rem' }}
+        />
+        <Column
+          field='cust_visit'
+          header='방문횟수'
+          sortable
+          style={{ minWidth: '12rem' }}
+        />
+        <Column
+          headerStyle={{ width: '5rem', textAlign: 'center' }}
+          bodyStyle={{ textAlign: 'center', overflow: 'visible' }}
+        />
+      </DataTable>
+    </div>
   );
 }
 
