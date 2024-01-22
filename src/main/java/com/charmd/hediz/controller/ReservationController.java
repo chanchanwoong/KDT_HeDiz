52a91bd697814b491e6c8edf667837188c2b9ace
Chanwoong Park <cksdnddlek99@gmail.com>
2024-01-22 13:45:44 +0900

    Refactor: HomeController 리팩토링

---
+++ src/main/java/com/charmd/hediz/controller/ReservationController.java
@@ -0,0 +1,16 @@
+package com.charmd.hediz.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+//    @Autowired
+//    private ReservationService reservationService;
+
+
+}
