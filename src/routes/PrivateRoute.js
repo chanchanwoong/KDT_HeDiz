514d65d2a5794192dd147bd9385f217c0cbba690
dev2gahui <dev2gahui@gmail.com>
2024-01-29 17:00:31 +0900

    Feat: route 기능 추가

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Toast } from 'primereact/toast';
 
 export default function PrivateRoute({ element, path }) {
   const navigate = useNavigate();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,68 +1,42 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
-// 로그인 페이지
-import AuthLayout from '../layout/AuthLayout';
-import SignIn from '../pages/auth/SignIn';
-import SignUp from '../pages/auth/SignUp';
+// 서비스 페이지
+import MainLayout from 'pages/layout/MainLayout';
+import Home from 'pages/Home';
+import Hairshop from 'pages/Hairshop';
+import HairStyle from 'pages/Hairstyle';
+import Schedule from 'pages/Schedule';
+import Reservation from 'pages/Reservation';
+import Payment from 'pages/Payment';
+import MyPage from 'pages/MyPage';
 
-// 메인화면
-import MainLayout from '../layout/MainLayout';
-import Home from '../pages/home/Home';
-import Reservation from '../pages/reservation/CheckReservation';
-
-//헤어샵 페이지
-import HairshopDetailPage from '../pages/hairshop/HairshopDetailPage';
-import HairstyleDetailPage from '../pages/hairshop/HairstyleDetailPage';
-import HairshopReservation from '../pages/hairshop/HairshopReservation';
-import HairshopPayment from '../pages/hairshop/HairshopPayment';
-
-//마이페이지
-import Mypage from '../pages/mypage/Mypage';
-import MyReservation from '../pages/mypage/MyReservation';
-import MyReview from '../pages/mypage/MyReview';
-import WriteReview from '../pages/mypage/WriteReview';
-
-//예약 페이지
+// 인증 페이지
+import SignIn from 'pages/auth/SignIn';
+import SignUp from 'pages/auth/SignUp';
 
 const router = createBrowserRouter([
-  {
-    path: '/auth',
-    element: <AuthLayout />,
-    children: [
-      { path: 'sign-in', element: <SignIn /> },
-      { path: 'sign-up', element: <SignUp /> },
-    ],
-  },
-
   {
     path: '/',
     element: <MainLayout />,
     children: [
-      { path: '', element: <Home /> },
-      { path: 'hairshop', element: <HairshopDetailPage /> },
-      { path: 'hairstyle', element: <HairstyleDetailPage /> },
-      { path: 'hairshop/reservation', element: <HairshopReservation /> },
-      { path: 'hairshop/payment', element: <HairshopPayment /> },
+      { path: '/', element: <Home /> },
+      { path: 'hairshop/:shop_seq', element: <Hairshop /> },
       {
-        path: 'mypage',
-        element: <Mypage />,
+        path: 'hairshop/:shop_seq/hairstyle/:style_seq',
+        element: <HairStyle />,
       },
-      {
-        path: 'mypage/list',
-        element: <MyReservation />,
-      },
-      {
-        path: 'mypage/review',
-        element: <MyReview />,
-      },
-      {
-        path: 'mypage/write-review',
-        element: <WriteReview />,
-      },
-      {
-        path: 'reservation',
-        element: <Reservation />,
-      },
+      { path: 'schedule', element: <Schedule /> },
+      { path: 'reservation', element: <Reservation /> },
+      { path: 'payment', element: <Payment /> },
+      { path: 'mypage', element: <MyPage /> },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <MainLayout />,
+    children: [
+      { path: 'sign-in', element: <SignIn /> },
+      { path: 'sign-up', element: <SignUp /> },
     ],
   },
 ]);
