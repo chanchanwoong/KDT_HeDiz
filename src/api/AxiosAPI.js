201da122cd1d4f06df891f4c34687736068eeecc
gahui-dev <dev2gahui@gmail.com>
2024-01-30 23:22:04 +0900

    Feat: AxiosAPI 수정

--- src/api/AxiosAPI.js
+++ src/api/AxiosAPI.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const createAxiosInstance = (headers = {}, options = {}) => {
   return axios.create({
     // baseURL: process.env.REACT_APP_AXIOS_BASE_URL || 'http://localhost:8090',
-    baseURL: process.env.REACT_APP_AXIOS_BASE_URL || '/hediz_client_server',
+    baseURL:
+      process.env.REACT_APP_AXIOS_BASE_URL || ':8090/hediz-client-server',
     headers: {
       ...headers,
     },
