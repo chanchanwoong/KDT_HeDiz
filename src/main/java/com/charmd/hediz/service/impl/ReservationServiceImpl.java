cb67eccbd4a7bbed234409cddd1a6723b2b91082
Chanwoong Park <cksdnddlek99@gmail.com>
2024-02-05 15:08:30 +0900

    Refactor: 불필요 코드 삭제

--- src/main/java/com/charmd/hediz/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/charmd/hediz/service/impl/ReservationServiceImpl.java
@@ -22,49 +22,6 @@ public class ReservationServiceImpl implements ReservationService {
         return dao.currentReservation(cust_seq);
     }
 
-    // reserv_stat : 0 -> 2
-    // receipt_id : 결제 영수증 id -> "cancel"
-    // pay_stat : 0 -> 1
-//    @Override
-//    @Transactional
-//    public int cancelReservation(int reserv_seq, String receipt_id) {
-//        String extracted_text = receipt_id.replace("{", "").replace("}", ""); // 중괄호 제거
-//        String[] arr = extracted_text.split(":");  // 세미콜론 기준 문자열 나누기
-//        String result = arr[1].replaceAll("\"", ""); // 빈 문자열 "" 제거
-//
-//
-//        // receipt_id를 통해 결제취소 요청
-//        HashMap<String, Object> res = null;
-//        int numberOfCancel = 0;
-//        int numberOfReservStat = 0;
-//        int numberOfPayStat = 0;
-//        try {
-//            Bootpay bootpay = new Bootpay("65af183ce57a7e001b410f16", "McUesnjacysjVSlaFBsWJL/fZqD3GUcQq1v8SbXplzQ=");
-//            HashMap<String, Object> token = bootpay.getAccessToken();
-//            if (token.get("error_code") != null) { //failed
-//                System.out.println("토큰 에러 발생");
-//            }
-//            Cancel cancel = new Cancel();
-//            cancel.receiptId = result;
-//            cancel.cancelUsername = "HeDiz";
-//            cancel.cancelMessage = "예약 취소";
-//            res = bootpay.receiptCancel(cancel);
-//            if (res.get("error_code") == null) { //success
-//                System.out.println("receiptCancel success: " + res);
-//                numberOfCancel++;
-//                // reserv_stat을 0에서 2로 변경 && receipt_id를 "cancle" 변경
-//                numberOfReservStat = dao.changeReservStat(reserv_seq);
-//                // pay_stat : 0 -> 1
-//                numberOfPayStat = dao.changePayStat(reserv_seq);
-//            } else {
-//                System.out.println("receiptCancel false: " + res);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return numberOfCancel + numberOfReservStat + numberOfPayStat;
-//    }
-
     @Override
     @Transactional
     public int cancelReservation(int reserv_seq, String receipt_id) {
