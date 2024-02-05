d101c67e655979d775bf7caf3c6a82a64c749032
gahui-dev <dev2gahui@gmail.com>
2024-02-06 05:16:37 +0900

    Feat: 사이드 바 수정

--- src/components/layout/AppSidebar.js
+++ src/components/layout/AppSidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from 'components/common/Logo';
 import { Menu } from 'primereact/menu';
